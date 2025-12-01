e718b29c6c88a0e40f6f57b356d9e472aec8287b
elllizzzzza <egalstyan@griddynamics.com>
2025-12-01 10:47:04 +0400

    Adding JDBC to the capstone project

---
+++ src/main/java/com/db/JDBCUtil.java
@@ -0,0 +1,95 @@
+package com.db;
+
+import java.sql.*;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class JDBCUtil {
+
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public JDBCUtil(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public void execute(String query, Object... args) {
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            fill(ps, args);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void execute(String query, Consumer<PreparedStatement> consumer) {
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            consumer.accept(ps);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T findOne(String query, Function<ResultSet, T> mapper, Object... args) {
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            fill(ps, args);
+            ResultSet rs = ps.executeQuery();
+
+            if (!rs.next()) return null;
+
+            T result = mapper.apply(rs);
+
+            if (rs.next()) {
+                throw new IllegalStateException("Query returned more than 1 row");
+            }
+
+            return result;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> findMany(String query, Function<ResultSet, T> mapper, Object... args) {
+        List<T> list = new ArrayList<>();
+
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            fill(ps, args);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(mapper.apply(rs));
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void fill(PreparedStatement ps, Object... args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            ps.setObject(i + 1, args[i]);
+        }
+    }
+}
