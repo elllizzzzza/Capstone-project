e96aa2c0631bc6bf1853b166f13e499e4e4732b6
elllizzzzza <egalstyan@griddynamics.com>
2026-01-09 15:51:19 +0400

    Added test

---
+++ src/test/java/com/educationalSystem/repository/BookRepoImplTest.java
@@ -0,0 +1,31 @@
+package com.educationalSystem.repository;
+
+import com.educationalSystem.entity.parts.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+@SpringBootTest
+class BookRepoImplTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void addBook_shouldStoreBook() {
+        Book book1 = new Book(1L, "Effective Java", "Joshua Bloch", "English", "Programming", true);
+
+        bookRepository.add(book1);
+
+        assertEquals(1, bookRepository.findAll().size());
+    }
+
+    @Test
+    void repository_shouldBeEmptyInitially() {
+        assertTrue(bookRepository.findAll().isEmpty());}
+}
