76306dabb640f53a76f78e21cad62df7628f6edf
elllizzzzza <egalstyan@griddynamics.com>
2026-03-06 16:52:00 +0400

    draft pr

--- course-service/src/main/java/com/educationalSystem/client/UserClient.java
+++ course-service/src/main/java/com/educationalSystem/client/UserClient.java
@@ -1,34 +1,21 @@
 package com.educationalSystem.client;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class UserClient {
 
-    private final WebClient.Builder webClientBuilder;
-    private final DiscoveryClient discoveryClient;
-
-    private String getUserServiceUrl() {
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
-        if (instances.isEmpty()) {
-            throw new RuntimeException("user-service is not available");
-        }
-        return instances.get(0).getUri().toString();
-    }
+    private final WebClient webClientBuilder;
 
     public boolean userExists(Long userId) {
         try {
-            webClientBuilder.build()
+            webClientBuilder
                     .get()
-                    .uri(getUserServiceUrl() + "/api/users/" + userId)
+                    .uri("http://user-service/api/users/" + userId)
                     .retrieve()
                     .bodyToMono(Object.class)
                     .block();
--- course-service/src/main/java/com/educationalSystem/config/WebClientConfig.java
+++ course-service/src/main/java/com/educationalSystem/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.educationalSystem.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +9,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder.build();
     }
 }
